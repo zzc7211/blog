cb1c493b26560ef1bc39b121b171f7fceec80942
zhouzichun <zhouzichun@xiaomi.com>
2022-01-18 12:59:48 +0800

    deploy test

--- deploy.sh
+++ deploy.sh
@@ -9,8 +9,6 @@ set -e
 # 进入生成的文件夹
 cd docs/.vuepress/dist
 
-# deploy to github pages
-# echo 'b.xugaoyi.com' > CNAME
 git config --global user.name "zhouzichun"
 git config --global user.email "914880597@qq.com"
 
@@ -18,32 +16,17 @@ git init
 git add -A
 git commit -m "init"
 git push -f https://zzc777:dcdd30ab3ea720c64d1ab9588fc08e7e@gitee.com/zzc777/zzc777.git master
-# git push -f https://zhouzichun:58920cf0649c21f08d699647cc2855f9@gitee.com/zhouzichun/zhouzichun.git master
-# if [ -z "$GITHUB_TOKEN" ]; then
-#   msg='deploy'
-#   githubUrl=git@github.com:zzc7211/blog.git
-# else
-# # https://github.com/zzc7211/blog.git
-#   msg='来自github actions的自动部署'
-#   githubUrl=https://zzc7211:${GITHUB_TOKEN}@github.com/zzc7211/blog.git
-# fi
-# git init
-# git add -A
-# git commit -m "${msg}"
-# git push -f $githubUrl master:gh-pages # 推送到github gh-pages分支
 
-# deploy to coding pages
-# echo 'www.xugaoyi.com\nxugaoyi.com' > CNAME  # 自定义域名
-# echo 'google.com, pub-7828333725993554, DIRECT, f08c47fec0942fa0' > ads.txt # 谷歌广告相关文件
-
-# if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
-#   codingUrl=git@e.coding.net:xgy/xgy.git
-# else
-#   codingUrl=https://HmuzsGrGQX:${CODING_TOKEN}@e.coding.net/xgy/xgy.git
-# fi
-# git add -A
-# git commit -m "${msg}"
-# git push -f $codingUrl master # 推送到coding
 
 cd -
 rm -rf docs/.vuepress/dist
+
+# 生成静态文件
+npm run build
+# 进入生成的文件夹
+cd docs/.vuepress/dist
+
+git init
+git add -A
+git commit -m "init"
+git push -f https://zhouzichun:58920cf0649c21f08d699647cc2855f9@gitee.com/zhouzichun/zhouzichun.git master
